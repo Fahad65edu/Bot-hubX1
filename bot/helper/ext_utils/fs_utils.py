a218fef5ccfc4f1f4c259ea30c99386acd4a674c
I AM YOU <92245201+Fahad65edu@users.noreply.github.com>
2022-05-12 11:47:01 +0600

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -1,12 +1,13 @@
-import sys
-import shutil
-import os
-import pathlib
-import magic
-import tarfile
-import subprocess
-import time
-import ceil
+
+from os import remove as osremove, path as ospath, mkdir, walk, listdir, rmdir
+from sys import exit as sysexit
+from json import loads as jsnloads
+from shutil import rmtree
+from PIL import Image
+from magic import Magic
+from subprocess import run as srun, check_output
+from time import time
+from math import ceil
 
 from PIL import Image
 from hachoir.parser import createParser
